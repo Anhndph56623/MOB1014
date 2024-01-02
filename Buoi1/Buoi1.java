998299aeba32e7f97083a47cf1dcc49c257ae14d
Anhndph56623 <Nguyenducanh221005@gmail.com>
2024-01-03 01:03:52 +0700

    up code

--- Buoi1/Buoi1.java
+++ Buoi1/Buoi1.java
@@ -1,5 +1,5 @@
 package MOB1014.Buoi1;
-
+// buoi 1 in ra man hinh 
 public class Buoi1 {
     public static void main(String[] args) {
         System.out.println("Buoi1");
--- buoi2/buoi2.java
+++ buoi2/buoi2.java
@@ -7,10 +7,5 @@ public class buoi2 {
 
     public static void main(String[] args) {
         System.out.println("aaaaaaaaaaaaa ");
-        System.out.println("aaaaaaaaaaaaa ");
-        System.out.println("aaaaaaaaaaaaa ");
-        System.out.println("aaaaaaaaaaaaa ");
-        System.out.println("aaaaaaaaaaaaa ");
-
     }
 }
