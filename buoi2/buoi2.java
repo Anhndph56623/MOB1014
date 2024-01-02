f44a8500d90c6d9a4b05863caa047e5b1ad207b5
Anhndph56623 <Nguyenducanh221005@gmail.com>
2024-01-03 00:55:48 +0700

    updatecode

--- buoi2/buoi2.java
+++ buoi2/buoi2.java
@@ -6,6 +6,11 @@ package MOB1014.buoi2;
 public class buoi2 {
 
     public static void main(String[] args) {
-        System.out.println("buoi222222 ");
+        System.out.println("aaaaaaaaaaaaa ");
+        System.out.println("aaaaaaaaaaaaa ");
+        System.out.println("aaaaaaaaaaaaa ");
+        System.out.println("aaaaaaaaaaaaa ");
+        System.out.println("aaaaaaaaaaaaa ");
+
     }
 }
